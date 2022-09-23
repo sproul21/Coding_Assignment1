6caf41399a43ad2a9f02187a274d14b718b3a41d
sproul21 <88341959+sproul21@users.noreply.github.com>
2022-09-22 22:51:48 -0400

    First attempt: reads in file, separates the headers, separates rest of data by line.

---
+++ main.py
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+def main():
+    with open('meteorite_landings_data.txt', 'r') as data:
+
+        list_of_items = []
+        for item in data:
+            stripped_line = item.strip()
+            line_list = stripped_line.split('\t')
+            list_of_items.append(line_list)
+
+        for i in list_of_items[0]:
+            header = i.split()
+            print(header)
+
+    print(list_of_items)
+
+class MeteorDataEntry():
+    def __init__(self, header):
+        self.header = header
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    main()
