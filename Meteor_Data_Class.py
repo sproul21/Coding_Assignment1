027a14078d8e4c95a3550b4574640218bc234c12
sproul21 <josh.sproul@gmail.com>
2022-09-27 19:54:27 -0400

    final commit

--- Meteor_Data_Class.py
+++ Meteor_Data_Class.py
@@ -1,14 +1,26 @@
 class MeteorDataEntry:
-    def __innit__(self, name, id, nametype, recclass, mass, fall, year, reclat, reclong, GeoLocation, States, Counties):
-        self.name = name
-        self.identification = id
-        self.name_type = nametype
-        self.rec_class = recclass
-        self.mass_g = mass
-        self.fall = fall
-        self.year_of_meteor = year
-        self.reclat_lattitude = reclat
-        self.reclong_longitude = reclong
-        self.geo_location = GeoLocation
-        self.state_found = States
-        self.counties_found = Counties
+    def __init__(self, meteor_data):
+
+        self.name = meteor_data[0]
+        self.identification = meteor_data[1]
+        self.name_type = meteor_data[2]
+        self.rec_class = meteor_data[3]
+        self.mass_g = meteor_data[4]
+        self.fall = meteor_data[5]
+        self.year_of_meteor = meteor_data[6]
+        self.reclat_lattitude = meteor_data[7]
+        self.reclong_longitude = meteor_data[8]
+        self.geo_location = meteor_data[9]
+        self.state_found = meteor_data[10]
+        self.counties_found = meteor_data[11]
+
+
+    def printdata(self):
+
+        print(self.name)
+
+
+ #   def mass_filter(self):
+  #      if self.mass_g.isnumeric():
+   #         if self.mass_g > 2900000
+    #            list.append(self)
--- main.py
+++ main.py
@@ -1,23 +1,44 @@
 
 
 
-
-
+from Meteor_Data_Class import*
 
 def main():
     with open('meteorite_landings_data.txt', 'r') as data:
 
         list_of_items = []
+        mass_list = []
+        date_list = []
         for item in data:
-            stripped_line = item.strip()
+            stripped_line = item.strip('\n')
             line_list = stripped_line.split('\t')
-            list_of_items.append(line_list)
+            list_of_items.append(MeteorDataEntry(line_list))
 
-        for i in list_of_items[0]:
-            header = i.split()
-            print(header)
+        for i in list_of_items:
 
-    print(list_of_items)
+            if i.mass_g.isnumeric():
+                 if int(i.mass_g) > 2900000:
+                    mass_list.append(i)
+
+        name_label = 'NAME'
+        mass_label = 'MASS (g)'
+        print(f'{name_label:<24}{mass_label:<20}')
+        print('========================================')
+        for i in mass_list:
+            print(f'{i.name:<24}{i.mass_g:<20}')
+
+        for i in list_of_items:
+
+            if i.year_of_meteor.isnumeric():
+                 if int(i.year_of_meteor) >= 2013:
+                    date_list.append(i)
+
+        name_label = 'NAME'
+        year_label = 'YEAR'
+        print(f'{name_label:<24}{year_label:<20}')
+        print('========================================')
+        for i in date_list:
+            print(f'{i.name:<24}{i.year_of_meteor:<20}')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
